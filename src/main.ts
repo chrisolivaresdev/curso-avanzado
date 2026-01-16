09443b05591d0fe03c6a67d71e9b7882fd925c57
christopher <chrisolivaresdev@gmail.com>
2026-01-16 16:14:12 -0400

    ssr

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { provideZoneChangeDetection } from "@angular/core";
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers]}).catch((err) =>
+bootstrapApplication(AppComponent, {...appConfig, providers: [provideZoneChangeDetection(), ...appConfig.providers, provideClientHydration(withEventReplay())]}).catch((err) =>
   console.error(err),
 );
